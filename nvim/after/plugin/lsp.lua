a1b0f3faa8015bce85196f10f245257389b7a226
sandeshkhadka <sandeshkhadka217@gmail.com>
2023-07-31 23:02:49 +0545

    telescope magics

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -22,7 +22,12 @@ lsp.configure("lua_ls", {
 lsp.configure("grammarly", {
 	root_dir = nvim_lsp.util.root_pattern("rmd"),
 })
-
+lsp.configure("clangd", {
+  cmd = {
+    "clangd",
+    "--offset-encoding=utf-16",
+  },
+})
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
@@ -55,14 +60,14 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+	-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+	-- vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
 	vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
 	vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
 	vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
 	vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+	-- vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
 	vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
 end)
 
--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,18 +1,25 @@
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>km", builtin.keymaps, {})
-vim.keymap.set("n", "<leader>pf", builtin.git_files, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>km", builtin.keymaps, { desc = "List all keymaps" })
+vim.keymap.set("n", "<leader>tgf", builtin.git_files, { desc = "Telescope git files" })
+vim.keymap.set("n", "<leader>tb", builtin.builtin, { desc = "Telescope builtin" })
+vim.keymap.set("n", "<leader>tld", builtin.diagnostics, { desc = "List all Diagnostic" })
+vim.keymap.set("n", "<leader>tws", builtin.lsp_dynamic_workspace_symbols, { desc = "Workspace Symbols" })
+vim.keymap.set("n", "<leader>tds", builtin.lsp_document_symbols, { desc = "Document Symbols" })
+vim.keymap.set("n", "<leader>tlg", builtin.live_grep, { desc = "Live Grep" })
+vim.keymap.set("n", "<leader>tlr", builtin.lsp_references, { desc = "LSP References" })
+vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "LSP Definitions" })
+
 vim.keymap.set("n", "<leader>ps", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 local actions = require("telescope.actions")
-
 require("telescope").setup({
-	defaults = {
-		mappings = {
-			i = {
-				["<M-i>"] = actions.close,
-			},
-		},
-	},
+  defaults = {
+    mappings = {
+      i = {
+        ["<M-i>"] = actions.close,
+      },
+    },
+  },
 })
