0a59b91bc1bc28fe071cd03f0a002253b181d114
sandeshkhadka <pqrsandesh@gmail.com>
2024-09-11 13:00:16 +0545

    switch to wayland

--- bin/config-editor.sh
+++ bin/config-editor.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 configs=(
-    'nvim' 'sxhkd' 'bspwm' 'alacritty' 'ranger' 'rofi' 
+    'nvim' 'sxhkd' 'swhkd' 'bspwm' 'alacritty' 'ranger' 'rofi' 
     'polybar' 'tmux' 'zathura' 'picom' 'starship'
 )
 
 file=$(for config in ${configs[@]}
 do
     echo "$config"
-done | rofi -dmenu)
+done | wofi -dmenu)
 case "$file" in
     "picom") alacritty -e nvim ~/.config/picom.conf
     ;;
     "starship") alacritty -e nvim ~/.config/starship.toml
+    ;;
+    "swhkd") alacritty -e sudo nvim /etc/swhkd/swhkdrc
     ;;
     "") exit
     ;;
